48f3c706497658e3da0e28e2623de5938efb6b76
Alex Yuan <alex@alexyuan.me>
2025-01-24 16:36:32 -0500

    formatting

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,6 +1,6 @@
 import Markdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
 
 export function Message(props) {
@@ -11,26 +11,29 @@ export function Message(props) {
       <div
         className={`prose max-w-none dark:prose-invert overflow-x-auto ${message.role === "assistant" ? "" : "px-5 py-3 bg-zinc-50 dark:bg-zinc-800 rounded-xl ml-auto"}`}
       >
-        <Markdown components={{
-          code(props) {
-            const { children, className, node, ...rest } = props
-            const match = /language-(\w+)/.exec(className || '')
-            return match ? (
-              <SyntaxHighlighter
-                {...rest}
-                className="not-prose"
-                PreTag="div"
-                children={String(children).replace(/\n$/, '')}
-                language={match[1]}
-                style={atomDark}
-              />
-            ) : (
-              <code {...rest} className={className}>
-                {children}
-              </code>
-            )
-          }
-        }} remarkPlugins={[remarkGfm]}>
+        <Markdown
+          components={{
+            code(props) {
+              const { children, className, node, ...rest } = props;
+              const match = /language-(\w+)/.exec(className || "");
+              return match ? (
+                <SyntaxHighlighter
+                  {...rest}
+                  className="not-prose"
+                  PreTag="div"
+                  children={String(children).replace(/\n$/, "")}
+                  language={match[1]}
+                  style={atomDark}
+                />
+              ) : (
+                <code {...rest} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+          remarkPlugins={[remarkGfm]}
+        >
           {message.content}
         </Markdown>
       </div>
