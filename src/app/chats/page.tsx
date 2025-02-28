81fe2987fb63ecbe75e086c4048205d31e2bf3ad
Alex Yuan <alex@alexyuan.me>
2025-02-27 21:32:10 -0500

    fix: switch chats to client

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,33 +1,21 @@
-import { auth } from "@clerk/nextjs/server";
+"use client";
+
 import { Card } from "@radix-ui/themes";
 import { formatDistanceToNow } from "date-fns";
-import { notFound } from "next/navigation";
+import useSWR from "swr";
 
-import { prisma } from "@/utils/prisma";
 import Link from "next/link";
 
-export default async function Chats() {
-  const { userId } = await auth();
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  if (!userId) {
-    return notFound();
-  }
-
-  const threads = await prisma.thread.findMany({
-    where: {
-      userId,
-      isDeleted: false,
-    },
-    orderBy: {
-      id: "desc",
-    },
-  });
+export default function Chats() {
+  const { data: threads } = useSWR("/api/threads", fetcher);
 
   return (
     <div className="p-4 w-2xl mx-auto my-10">
       <p className="text-2xl mb-5">Recent chats</p>
       <div className="flex flex-col space-y-2">
-        {threads.map((thread) => (
+        {threads?.map((thread) => (
           <Link key={thread.token} href={`/chat/${thread.token}`}>
             <Card>
               <p className="font-medium mb-1">{thread.name}</p>
