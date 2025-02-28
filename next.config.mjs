0891a21239cc6b088f85dbc9e0f611c6d9208d64
Alex Yuan <alex@alexyuan.me>
2025-02-28 18:22:09 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  cleanDistDir: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
