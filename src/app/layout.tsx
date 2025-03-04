3acbc7f07df02e3864427330d4074f0535e4839c
Alex Yuan <alex@alexyuan.me>
2025-03-04 01:50:41 -0500

    fix: dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider, SignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import { Badge, Theme, ThemePanel } from "@radix-ui/themes";
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import { Instrument_Serif, Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 
@@ -39,35 +40,37 @@ export default function RootLayout({
         </head>
         <body className={`${inter.variable} ${instrument_serif.variable}`}>
           <Toaster position="bottom-right" />
-          <Theme accentColor="gray" radius="large">
-            <SignedOut>
-              <div className="flex w-screen h-screen">
-                <div className="flex justify-center items-center mx-auto w-1/3 bg-zinc-100">
-                  <SignIn />
-                </div>
-                <div className="p-10 grow">
-                  <div className="flex items-center space-x-4 mb-5">
-                    <img className="w-16 h-16" src="/cedar.svg" />
-                    <p className="text-6xl font-semibold">Cedar</p>
-                    <Badge className="uppercase" color="blue">
-                      Closed Alpha
-                    </Badge>
+          <ThemeProvider attribute="class">
+            <Theme accentColor="gray" radius="large">
+              <SignedOut>
+                <div className="flex w-screen h-screen">
+                  <div className="flex justify-center items-center mx-auto w-1/3 bg-zinc-100">
+                    <SignIn />
+                  </div>
+                  <div className="p-10 grow">
+                    <div className="flex items-center space-x-4 mb-5">
+                      <img className="w-16 h-16" src="/cedar.svg" />
+                      <p className="text-6xl font-semibold">Cedar</p>
+                      <Badge className="uppercase" color="blue">
+                        Closed Alpha
+                      </Badge>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </SignedOut>
-            <SignedIn>
-              <div className="flex w-screen h-screen bg-zinc-100 dark:bg-zinc-950">
-                <Sidebar />
-                <div className="bg-white dark:bg-zinc-900 grow overflow-y-auto">
-                  {children}
+              </SignedOut>
+              <SignedIn>
+                <div className="flex w-screen h-screen bg-zinc-100 dark:bg-zinc-950">
+                  <Sidebar />
+                  <div className="bg-white dark:bg-zinc-900 grow overflow-y-auto">
+                    {children}
+                  </div>
                 </div>
-              </div>
-            </SignedIn>
-            {process.env.NODE_ENV === "development" && (
-              <ThemePanel defaultOpen={false} />
-            )}
-          </Theme>
+              </SignedIn>
+              {process.env.NODE_ENV === "development" && (
+                <ThemePanel defaultOpen={false} />
+              )}
+            </Theme>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
