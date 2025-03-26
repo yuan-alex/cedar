337e2bff46e996adbe2702cd9d2a67913ed31403
Alex Yuan <alex@alexyuan.me>
2025-03-26 13:37:30 -0400

    update: landing page

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -3,23 +3,22 @@ import { Button } from "@radix-ui/themes";
 
 export function LandingPage() {
   return (
-    <div className="min-h-screen bg-zinc-100 dark:bg-black pt-5">
-      <nav className="p-5 bg-white dark:bg-zinc-900 rounded-xl shadow flex items-center mx-10">
-        <div className="flex items-center space-x-3">
-          <img className="w-10 h-10" src="/cedar.svg" />
-          <p className="text-4xl font-medium">Cedar</p>
-        </div>
-        <div className="grow" />
-      </nav>
-      <p className="text-5xl md:text-8xl font-bold text-center mt-20">
-        The Best AI Chatbot
-      </p>
-      <p className="text-2xl md:text-4xl text-center text-zinc-500 mt-5">
-        Cedar is the AI workspace built for productivity.
-      </p>
-      <div className="flex justify-center mt-10">
+    <div className="min-h-screen bg-zinc-100 dark:bg-black">
+      <div className="flex justify-center items-center space-x-3 py-10">
+        <img className="w-10 h-10" src="/cedar.svg" />
+        <p className="text-4xl font-medium">Cedar</p>
+      </div>
+      <div className="p-50 mx-10 text-center bg-white shadow rounded-xl">
+        <p className="text-5xl md:text-7xl font-bold mb-8">
+          The Power User's LLM
+        </p>
+        <p className="text-2xl md:text-3xl text-zinc-500 mb-8">
+          The enterprise-ready AI platform built for productivity.
+        </p>
         <SignInButton>
-          <Button size="4">Sign in</Button>
+          <Button size="4" variant="outline">
+            Sign in
+          </Button>
         </SignInButton>
       </div>
     </div>
