33d71a645720b539bbc327bb228dd0e23f3c3431
Alex Yuan <alex@alexyuan.me>
2025-05-01 02:56:44 -0400

    fix: i am foolish

--- server/bun.ts
+++ server/bun.ts
@@ -1,15 +1,14 @@
+import { serve } from "bun";
 import { serveStatic } from "hono/bun";
+
 import { app } from "./main";
 
 if (process.env.NODE_ENV === "production") {
   app.get("/*", serveStatic({ root: "./dist/client" }));
 }
 
-if (process.env.NODE_ENV === "development") {
-  const { serve } = await import("bun");
-  const server = serve({
-    fetch: app.fetch,
-    port: Number.parseInt(process.env.PORT || "3001"),
-  });
-  console.log(`Listening on ${server.url}`);
-}
+const server = serve({
+  fetch: app.fetch,
+  port: Number.parseInt(process.env.PORT || "3001"),
+});
+console.log(`Listening on ${server.url}`);
