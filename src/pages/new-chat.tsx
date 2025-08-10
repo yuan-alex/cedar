b248b6dbcd0590ef1fc414695e9671b935e663b7
Alex Yuan <alex@alexyuan.me>
2025-08-09 21:32:14 -0400

    update: new chat ui

--- src/pages/new-chat.tsx
+++ src/pages/new-chat.tsx
@@ -6,8 +6,12 @@ import { $model, $prompt } from "@/utils/stores";
 export function NewChat() {
   const navigate = useNavigate();
 
-  function handleCreateThread(event) {
+  function handleCreateThread(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    if (!$prompt.value) {
+      return;
+    }
 
     fetch("/api/v1/threads", {
       method: "POST",
@@ -30,20 +34,22 @@ export function NewChat() {
 
   return (
     <form className="h-full flex flex-col" onSubmit={handleCreateThread}>
-      <div className="grow flex items-center justify-center my-10">
-        <div className="max-w-2xl w-full">
-          <div className="flex items-center justify-center mb-5 space-x-4">
-            <img className="w-12 h-12" src="/images/cedar.svg" />
-            <p className="text-5xl font-medium">Cedar</p>
+      <div className="grow flex items-center justify-center">
+        <div className="max-w-4xl w-full px-4 md:px-0">
+          <div className="flex flex-col items-center justify-center mb-10">
+            <div className="flex items-center space-x-4 mb-4">
+              <img className="w-12 h-12" src="/images/cedar.svg" alt="Cedar" />
+              <p className="text-5xl font-medium">Cedar</p>
+            </div>
+          </div>
+          <div className="w-full">
+            <InputBox
+              name="message"
+              rows={3}
+              onChange={(event) => $prompt.set(event.target.value)}
+            />
           </div>
         </div>
-      </div>
-      <div className="w-full max-w-4xl mx-auto mb-2">
-        <InputBox
-          name="message"
-          rows={3}
-          onChange={(event) => $prompt.set(event.target.value)}
-        />
       </div>
     </form>
   );
