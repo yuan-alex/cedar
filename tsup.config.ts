0a45688bc7ea50655129b6024ee24f16edc55869
Alex Yuan <alex@alexyuan.me>
2025-09-11 13:54:11 -0400

    chore: remove unused

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["server/main.ts"],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  outDir: "dist/server",
-});
