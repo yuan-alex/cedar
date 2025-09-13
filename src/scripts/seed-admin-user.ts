2e1365c49ee0f9527e54987ca0fbbb08125b8cc6
Alex Yuan <alex@alexyuan.me>
2025-09-13 15:05:46 -0400

    feat: seed admin user

---
+++ src/scripts/seed-admin-user.ts
@@ -0,0 +1,25 @@
+import { auth } from "@/server/utils/auth";
+import prisma from "@/server/utils/prisma";
+
+const email = "admin@example.com";
+
+try {
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!process.env.BETTER_AUTH_SECRET || existingUser) {
+    process.exit(0);
+  }
+
+  const res = await auth.api.signUpEmail({
+    body: {
+      name: "admin",
+      email,
+      password: process.env.BETTER_AUTH_SECRET,
+    },
+  });
+  console.log("Sign up successful:", res);
+} catch (error) {
+  console.log("Sign up failed:", error);
+}
