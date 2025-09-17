d2e965c4c08568c60cc6f9ebc31763f513585614
Alex Yuan <alex@alexyuan.me>
2025-09-17 03:32:02 -0400

    fix: temp remove tanstack dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,27 +17,6 @@ function RootComponent() {
       <SignedOut>
         <Outlet />
       </SignedOut>
-      {process.env.NODE_ENV === "development" && <DevTools />}
     </BetterAuthUiProvider>
   );
-}
-
-function DevTools() {
-  try {
-    // Dynamic imports for dev dependencies to avoid build errors in production
-    const { ReactQueryDevtools } = require("@tanstack/react-query-devtools");
-    const {
-      TanStackRouterDevtools,
-    } = require("@tanstack/react-router-devtools");
-
-    return (
-      <>
-        <TanStackRouterDevtools position="top-right" />
-        <ReactQueryDevtools />
-      </>
-    );
-  } catch (error) {
-    // Dev tools not available, skip silently
-    return null;
-  }
 }
